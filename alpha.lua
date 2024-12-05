e4f3793000f4cf1b987f98c77ff4f2bd75161670
Matt <matt@1-800-rad-dude.com>
2024-12-05 18:34:08 -0500

    Update alpha, dedupe output

--- alpha.lua
+++ alpha.lua
@@ -7,11 +7,25 @@ function process(filename)
             words[#words+1] = word
         end
     end
+    words = dedupe(words)
     table.sort(words)
 
     for _, word in pairs(words) do
         print(word)
     end
+end
+
+function dedupe(t)
+    local found = {}
+    local results = {}
+    for _, value in pairs(t) do
+        if not found[value] then
+            found[value] = true
+            results[#results+1] = value
+        end
+    end
+
+    return results
 end
 
 filename = arg[1]
